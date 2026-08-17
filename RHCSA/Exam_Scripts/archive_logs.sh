b84912994d751d9261952f15f25653933fc7f5d6
Super User <root@ThinkPad-T14.localdomain>
2026-08-17 22:04:18 +0200

    fix: updated archive_logs.sh script with better logic

---
+++ RHCSA/Exam_Scripts/archive_logs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+if [[ "$EUID" -ne 0 ]]; then
+	echo "[ERROR] You have to be root";
+	exit 1
+fi
+
+if [[ -z "$1" ]]; then
+	echo "[ERROR] This script requires an argument"
+	echo "[$0 USAGE] <directory>"
+	exit 1
+fi
+
+if [[ ! -d "$1" ]]; then
+	echo "[ERROR] This directory $1 does not exist"
+	exit 1
+fi
+
+target_dir="$1"
+mkdir -p /backup
+dir_backup="/backup"
+
+find "$target_dir" -type f -name "*.log" -mtime -7 -exec cp {} /backup/ \;
+
+echo "[SUCCESS]"
+
+
